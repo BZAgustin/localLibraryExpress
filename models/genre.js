052e64c65ef539bc172c3abf378a2b0ea22f84d2
BZAgustin <abustoszanelli@gmail.com>
2023-09-12 10:09:14 -0300

    Add schemas

---
+++ models/genre.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const GenreSchema = new Schema({
+  name: { type: String, 
+          required: true, 
+          minlength: 3, 
+          maxlength: 100 }
+});
+
+GenreSchema.virtual('url').get(function() {
+  return `/catalog/genre/${this._id}`;
+});
+
+module.exports = mongoose.model('Genre', GenreSchema);
